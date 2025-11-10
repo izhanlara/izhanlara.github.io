2376ce9abe7205ab5555b605b69af9ffec235543
Izhan Lara <izhanlaragarcia@gmail.com>
2025-11-10 10:29:57 +0100

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 export default defineConfig({
   i18n: {
-    locales: ["en", "es", "de"],
+    locales: ["en", "es"],
     defaultLocale: "en",
     routing: "manual"
 }
